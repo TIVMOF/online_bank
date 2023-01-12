348a41330a7dcdf6e1a35ea4cbe1a99e3e91937c
tomyslav <tomyivanov06@gmail.com>
2023-01-12 23:08:31 +0200

    some_stf

--- lib/utill/my_card.dart
+++ lib/utill/my_card.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_interpolation_to_compose_strings, prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
 
