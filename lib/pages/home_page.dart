caa053870af39e632fe3e23bd2da789ee18aa842
tomyslav <tomyivanov06@gmail.com>
2023-01-24 22:32:56 +0200

    jkj

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,14 +8,13 @@ import 'package:online_bank/utill/my_list_tile.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:online_bank/pages/send_page.dart';
 
-import '../utill/my_bar.dart';
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
 
 class _HomePageState extends State<HomePage> {
   final _controller = PageController();
@@ -24,169 +23,201 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(children: [
-        MyBar(),
-        // bank name
-        Align(
-          alignment: Alignment.topLeft,
-          child: Container(
-            width: 600,
-            decoration: BoxDecoration(
-              color: Colors.blue.shade800,
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(95),
-              )
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 30.0, top: 10.0, bottom: 10.0),
+      bottomNavigationBar: BottomAppBar(
+              color: Colors.grey[200],
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Row(
-                    children: [
-                      Text(
-                        'Proper Invest',
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                  IconButton(
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => MyAbout()));
+                    }, 
+                    icon: Icon(Icons.question_answer),
+                    iconSize: 25,
+                    color: Colors.blue.shade900,
+                    ),
+                  IconButton(
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                    }, 
+                    icon: Icon(Icons.home),
+                    iconSize: 35,
+                    color: Colors.blue.shade900,
+                    ),
+                  IconButton(
+                    onPressed: (){
+                      Navigator.of(context).pop();
+                    }, 
+                    icon: Icon(Icons.arrow_back_ios),
+                    iconSize: 25,
+                    color: Colors.blue.shade900,
+                    ),
+              ],),
+            ),
+      body: SafeArea(
+        child: Column(children: [
+          // app bar
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              width: 600,
+              decoration: BoxDecoration(
+                color: Colors.blue.shade800,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(95),
+                )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30.0, top: 10.0, bottom: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Proper Invest',
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            ),
                           ),
-                        ),
-                      Text(
-                        ' Bank',
-                        style: TextStyle(
-                          fontSize: 28,
-                          color: Colors.white,
+                        Text(
+                          ' Bank',
+                          style: TextStyle(
+                            fontSize: 28,
+                            color: Colors.white,
+                            ),
                           ),
-                        ),
+                      ],
+                    ),
                     ],
                   ),
-                  ],
-                ),
+              ),
             ),
           ),
-        ),
-
-        SizedBox(height: 25,),
       
-        // cards
-        Container(
-          height: 200,
-          child: PageView(
-            scrollDirection: Axis.horizontal,
-            controller: _controller,
-            children: [
-              MyCard(
-                balance: 5250.20,
-                cardNumber: 12345678,
-                expiryDate: 4,
-                expiryMonth: 10,
-                expiryYear: 24,
-                color: Colors.deepPurple,
-              ),
-              MyCard(
-                balance: 4250.24,
-                cardNumber: 11345678,
-                expiryDate: 5,
-                expiryMonth: 11,
-                expiryYear: 23,
-                color: Colors.deepOrange,
-              ),
-              MyCard(
-                balance: 250.99,
-                cardNumber: 12355678,
-                expiryDate: 3,
-                expiryMonth: 12,
-                expiryYear: 25,
-                color: Colors.blue,
+          SizedBox(height: 25,),
+        
+          // cards
+          Container(
+            height: 200,
+            child: PageView(
+              scrollDirection: Axis.horizontal,
+              controller: _controller,
+              children: [
+                MyCard(
+                  balance: 5250.20,
+                  cardNumber: 12345678,
+                  expiryDate: 4,
+                  expiryMonth: 10,
+                  expiryYear: 24,
+                  color: Colors.deepPurple,
+                ),
+                MyCard(
+                  balance: 4250.24,
+                  cardNumber: 11345678,
+                  expiryDate: 5,
+                  expiryMonth: 11,
+                  expiryYear: 23,
+                  color: Colors.deepOrange,
+                ),
+                MyCard(
+                  balance: 250.99,
+                  cardNumber: 12355678,
+                  expiryDate: 3,
+                  expiryMonth: 12,
+                  expiryYear: 25,
+                  color: Colors.blue,
+                ),
+                
+            ],
+            )
+          ),
+      
+          SizedBox(height: 15),
+      
+          SmoothPageIndicator(
+            controller: _controller, 
+            count: 3,
+            effect: ExpandingDotsEffect(
+              activeDotColor: Colors.blue.shade900,
+            ),
+            ),
+      
+            SizedBox(height: 20),
+      
+          // 3 buttons ->send + pay + bills
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              // send button
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MySend()));
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.black,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                  ),
+                child: MyButton(iconImagePath: 'lib/icons/send.png', buttonText: 'Send',)
               ),
               
-          ],
-          )
-        ),
-
-        SizedBox(height: 20),
-
-        SmoothPageIndicator(
-          controller: _controller, 
-          count: 3,
-          effect: ExpandingDotsEffect(
-            activeDotColor: Colors.blue.shade900,
-          ),
-          ),
-
-          SizedBox(height: 40),
-
-        // 3 buttons ->send + pay + bills
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-            // send button
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => MySend()));
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  foregroundColor: Colors.black,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
-                ),
-              child: MyButton(iconImagePath: 'lib/icons/send.png', buttonText: 'Send',)
-            ),
-            
-            //pay button
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => MySend()));
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  foregroundColor: Colors.black,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
-                ),
-              child: MyButton(iconImagePath: 'lib/icons/credit-card.png', buttonText: 'Pay',)
-            ),
-
-            //bills button
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => MySend()));
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  foregroundColor: Colors.black,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
-                ),
-              child: MyButton(iconImagePath: 'lib/icons/bill.png', buttonText: 'Bills',)
-            ),
-          ],),
-        ),
+              //pay button
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MySend()));
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.black,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                  ),
+                child: MyButton(iconImagePath: 'lib/icons/credit-card.png', buttonText: 'Pay',)
+              ),
       
-        // column -> stats + transaction
-        Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(children: [
-            //Stats
-            MyListTile(
-              iconImagePath: 'lib/icons/statistics.png', 
-              tileTitle: 'Statistics', 
-              tileSubtitle: 'Payments and Income'
-            ),
-
-            //Transactions
-            MyListTile(
-              iconImagePath: 'lib/icons/lending.png', 
-              tileTitle: 'Transactions', 
-              tileSubtitle: 'Transaction History'
-            )
-          ],),
-        )
-      ],),
+              //bills button
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MySend()));
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.black,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                  ),
+                child: MyButton(iconImagePath: 'lib/icons/bill.png', buttonText: 'Bills',)
+              ),
+            ],),
+          ),
+        
+          // column -> stats + transaction
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(children: [
+              //Stats
+              MyListTile(
+                iconImagePath: 'lib/icons/statistics.png', 
+                tileTitle: 'Statistics', 
+                tileSubtitle: 'Payments and Income'
+              ),
+      
+              //Transactions
+              MyListTile(
+                iconImagePath: 'lib/icons/lending.png', 
+                tileTitle: 'Transactions', 
+                tileSubtitle: 'Transaction History'
+              )
+            ],),
+          )
+        ],),
+      ),
     );
   }
 }
--- lib/utill/my_button.dart
+++ lib/utill/my_button.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
