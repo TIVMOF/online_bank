ac7539f09e8af4c795753cd347b496572bfb320e
tomyslav <tomyivanov06@gmail.com>
2023-02-06 16:42:03 +0200

    bills page confining

--- lib/pages/bills_page.dart
+++ lib/pages/bills_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:online_bank/utill/app_bar.dart';
 
@@ -15,14 +17,35 @@ class BillsPage extends StatefulWidget {
 }
 
 class _BillsPageState extends State<BillsPage> {
+
+  final _controller = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       bottomNavigationBar: AppBarBottom(context: this.context),
       body: SafeArea(
-        child: Column(children: [
-          MyAppBar(first_name: 'Your', second_name: 'Bills')
+        child: Column(
+          children: [
+            MyAppBar(first_name: 'Your', second_name: 'Bills'),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25.0),
+              child: Container(
+                height: 200,
+                child: PageView(
+                  scrollDirection: Axis.vertical,
+                  controller: _controller,
+                  children: [
+                    Container(
+                      width: 500,
+                      color: Color.fromARGB(60, 0, 66, 117),
+                    )
+                  ],
+                ),
+              ),
+            )
         ],)
         ),
     );
