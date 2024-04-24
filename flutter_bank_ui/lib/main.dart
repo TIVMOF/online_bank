c2f47dbc6f1ca75b7d69cf2a4139ab9664021f90
TIVMOF <tomyivnov06@gmail.com>
2024-04-24 03:39:54 +0300

    Added login page

--- flutter_bank_ui/lib/main.dart
+++ flutter_bank_ui/lib/main.dart
@@ -1,25 +1,21 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'pages/home_page.dart';
+import 'pages/login_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
-    runApp(new MyApp());
+    runApp(MyApp());
   });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(), // Always start with the login page
     );
   }
 }
