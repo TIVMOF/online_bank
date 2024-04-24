deeb4333216e39b0854afaab732dbe5ecaac1d75
TIVMOF <tomyivanov06@gmail.com>
2024-04-24 21:28:44 +0000

    User api working

---
+++ dirigible-bank-server-api/user.js
@@ -0,0 +1,60 @@
+import { rs } from "@dirigible/http"
+const userDao = require("dirigible-bank-server/gen/dao/users/Users.js");
+// import { session } from "sdk/http";
+
+rs.service({
+    "login": {
+        "post": [
+            {
+                "serve": (_ctx, request, response) => {
+                    const body = request.getJSON();
+
+                    ["Username", "Password"].forEach((field) => {
+                        if (!(field in body)) {
+                            response.setStatus(400);
+                            response.println("Required more parameters");
+                            return;
+                        }
+                    });
+
+                    const settings = {
+                        where: {
+                            property: 'Username',
+                            operator: 'eq',
+                            value: body.Username,
+                        },
+                    };
+
+                    const users = userDao.list(settings);
+
+                    if (users && users.length > 0) {
+                        const user = users.find(user => user.Username === body.Username);
+
+                        if (user) {
+                            if (user.Password === body.Password) {
+
+                                response.setStatus(200);
+                                response.setContentType("application/json");
+                                response.println(JSON.stringify(user));
+                            } else {
+                                response.setStatus(401);
+                                response.println("Invalid password");
+                                return;
+                            }
+                        }
+                        else {
+                            response.setStatus(404); // User not found
+                            response.println("User not found");
+                            return;
+                        }
+                    }
+                },
+
+                "catch": (_ctx, err, _request, response) => {
+                    response.println("Pesho");
+                    response.println(err);
+                }
+            },
+        ],
+    }
+}).execute();
