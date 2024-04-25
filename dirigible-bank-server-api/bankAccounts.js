313490c3ff8ce3701178744a9084e5092dd8dc06
TIVMOF <tomyivanov06@gmail.com>
2024-04-25 01:34:29 +0000

    Added bankAccount api

---
+++ dirigible-bank-server-api/bankAccounts.js
@@ -0,0 +1,78 @@
+import { rs } from "@dirigible/http"
+const userDao = require("dirigible-bank-server/gen/dao/users/Users.js");
+const bankAccountDao = require("dirigible-bank-server/gen/dao/bankAccount/BankAccounts.js");
+
+rs.service({
+    "getAccountByUser": {
+        "get": [
+            {
+                "serve": (_ctx, request, response) => {
+                    const body = request.getJSON();
+
+                    if (!('Id' in body)) {
+                        response.setStatus(400);
+                        response.println("Required more parameters");
+                        return;
+                    }
+
+                    if (!userDao.get(body.Id)) {
+                        response.setStatus(404);
+                        response.println("Cannot find user!");
+                        return;
+                    }
+
+                    const allBankAccounts = bankAccountDao.list();
+
+                    const userAccounts = [];
+
+                    allBankAccounts.forEach(account => {
+                        if (account.Users == body.Id) {
+                            userAccounts.push(account);
+                        }
+                    })
+
+                    response.setStatus(200);
+                    response.setContentType("application/json");
+                    response.println(JSON.stringify(userAccounts));
+                },
+
+                "catch": (_ctx, err, _request, response) => {
+                    response.println(err);
+                }
+            },
+        ],
+    },
+    "getAccountByIBAN": {
+        "get": [
+            {
+                "serve": (_ctx, request, response) => {
+                    const body = request.getJSON();
+
+                    if (!('IBAN' in body)) {
+                        response.setStatus(400);
+                        response.println("Required more parameters");
+                        return;
+                    }
+
+                    const allBankAccounts = bankAccountDao.list();
+
+                    const iBANAccounts = [];
+
+                    allBankAccounts.forEach(account => {
+                        if (account.IBAN == body.IBAN) {
+                            iBANAccounts.push(account);
+                        }
+                    })
+
+                    response.setStatus(200);
+                    response.setContentType("application/json");
+                    response.println(JSON.stringify(iBANAccounts));
+                },
+
+                "catch": (_ctx, err, _request, response) => {
+                    response.println(err);
+                }
+            },
+        ],
+    }
+}).execute();
